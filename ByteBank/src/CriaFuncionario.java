5b396f5e2f210728df498f3f7781a8726807e835
Jeziel Almeida <jeziel.almeida16@gmail.com>
2022-05-09 17:25:03 +0000

    Java Alura

---
+++ ByteBank/src/CriaFuncionario.java
@@ -0,0 +1,15 @@
+public class CriaFuncionario {
+
+	public static void main(String[] args) {
+		Funcionario jezi = new Funcionario();
+		jezi.setNome("Jeziel");
+		jezi.setSalario(1000);
+		System.out.println(jezi.getNome() +" tem a bonificação de "+ jezi.getBonificacao());
+		
+		Gerente boni = new Gerente();
+		boni.setNome("Boni");
+		boni.setSalario(1000);
+		System.out.println(boni.getNome() +" tem a bonificação de "+ boni.getBonificacao());
+	}
+
+}
